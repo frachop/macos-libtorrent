e91eb502ba7b0de887afaae886bedea7f1b97c95
Frachop <franck.chopin@ymagis.com>
2018-05-13 10:46:58 +0200

    t03

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ bootstrap () {
 	sudo apt-get -y update
 	sudo apt-get -y install gcc-4.9 g++-4.9
 	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
-	sudo apt-get -y upgrade
 
 	mkdir -p "$DEPS_DIR_SRC"
 }
